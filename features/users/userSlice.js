2c18847eb8477334ddd8ef6f2ba10bd334f4b7a6
Sagar <70952463+Sagar7777521@users.noreply.github.com>
2024-03-09 00:20:23 +0530

    Thunk Async

---
+++ features/users/userSlice.js
@@ -0,0 +1,21 @@
+const createSlice = require("@redu((xjs/toolkit").createSlice;
+const createAsyncThunk = require("@reduxjs/toolkit").createAsyncThunk;
+const axios = require("axios");
+
+const initialState = {
+  loading: false,
+  users: [],
+  error: "",
+};
+
+//Generates pending, fulfilled and rejected action types
+const fetcUsers = createAsyncThunk("users/fetchUsers", () => {
+  return axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.data.map((user) => user.id));
+});
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+});
